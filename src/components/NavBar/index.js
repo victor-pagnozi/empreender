974d30db45d546a2e109d7b45d04691451d6250c
victor-pagnozi <victor.pagnozi@gmail.com>
2020-12-08 21:18:47 -0300

    Instalado biblioteca Styled-Components, criado component para exibir os anuncios, baixado logo para teste e criado a NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,12 +1,17 @@
 import React from "react";
-import "./styles.css";
+import logo from "../../assets/images/logo.jpg"
+import { Link } from "react-router-dom";
+import { Container } from "./styles";
 
-function NavBar(props){
-  <header>
-    <div className="nav-bar">
-      <span>NavBar</span>
-    </div>
-  </header>;
-};
+function NavBar() {
+  return (
+    <Container>
+      <Link className="logo" to="/"><img src={logo} alt="Logo do Site"/></Link>
+      <Link>Teste</Link>
+      <Link>Teste</Link>
+      <Link>Teste</Link>
+    </Container>
+  );
+}
 
 export default NavBar;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,27 @@
 import React from "react";
-import "./styles.css";
+import { Anuncio, Container, ImgDivulgacao, ContainerAnuncios } from "./styles";
 import NavBar from "../../components/NavBar";
+import Anuncios from "../../components/Anuncios";
 
-function Home(){
-    return(
-        <div>
-            <h1>Teste</h1>
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <NavBar></NavBar>
+
+      <Container>
+        <ImgDivulgacao>
+          <img
+            src="https://www.vrsys.com.br/images/vrsys/blog-new/o-que-sao-logotipos-e-porque-sua-empresa-precisa-de-um-imagem-avon.jpg"
+            alt=""
+          />
+        </ImgDivulgacao>
+      </Container>
+
+      <Container>
+        <Anuncios></Anuncios>
+      </Container>
+    </div>
+  );
 }
 
 export default Home;
